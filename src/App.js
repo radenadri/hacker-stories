62d865fee3d15aaf0395a6d0453f902842262d4d
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-08 23:10:17 +0700

    Wrapping search into components

--- src/App.js
+++ src/App.js
@@ -63,6 +63,23 @@ const Item = ({ item, onRemoveItem }) => {
   );
 };
 
+const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      label="Search"
+      value={searchTerm}
+      onInputChange={onSearchInput}
+    >
+      <strong>Search :</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
+
 const useSemiPersistenState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState);
 
@@ -121,8 +138,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
   };
 
   const handleFetchStories = useCallback(async () => {
@@ -155,18 +174,11 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        label="Search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search :</strong>
-      </InputWithLabel>
-
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
