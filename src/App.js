5e6e63f50ecd195589e0cc3e2ce4bf4b2aba7664
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-08 22:55:03 +0700

    Change fetching url using axios library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useReducer, useRef, useState } from "react";
+import axios from "axios";
 
 import "./styles.css";
 
@@ -127,12 +128,12 @@ const App = () => {
   const handleFetchStories = useCallback(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits
+          payload: result.data.hits
         });
       })
       .catch(() => {
