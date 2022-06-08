84b04bf3a8ecba4305982c67b940d63398a6746b
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-08 23:58:24 +0700

    Applying styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import { useCallback, useEffect, useReducer, useRef, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
-import styles from "./App.module.css";
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
 
 const InputWithLabel = ({
   id,
@@ -19,20 +44,31 @@ const InputWithLabel = ({
     }
   }, [isFocused]);
 
+  const StyledLabel = styled.label`
+    border-top: 1px solid #171212;
+    border-left: 1px solid #171212;
+    padding-left: 5px;
+    font-size: 24px;
+  `;
+
+  const StyledInput = styled.input`
+    border: none;
+    border-bottom: 1px solid #171212;
+    background-color: transparent;
+    font-size: 24px;
+  `;
+
   return (
     <>
-      <label htmlFor={id} className={styles.label}>
-        {children}
-      </label>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
-      <input
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         autoFocus={isFocused}
         onChange={onInputChange}
-        className={styles.input}
       />
     </>
   );
@@ -49,29 +85,43 @@ const List = ({ list, onRemoveItem }) => (
 const Item = ({ item, onRemoveItem }) => {
   const { url, title, author, num_comments, points } = item;
 
+  const StyledItem = styled.li`
+    display: flex;
+    align-items: center;
+    padding-bottom: 5px;
+  `;
+
+  const StyledColumn = styled.span`
+    padding: 0 5px;
+    white-space: nowrap;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    a {
+      color: inherit;
+    }
+    width: ${(props) => props.width};
+  `;
+
   return (
-    <li className={styles.item}>
-      <span style={{ width: "40%" }}>
+    <StyledItem>
+      <StyledColumn width="40%">
         <a href={url}>{title}</a>
-      </span>
-      <span style={{ width: "30%" }}>{author}</span>
-      <span style={{ width: "10%" }}>{num_comments}</span>
-      <span style={{ width: "10%" }}>{points}</span>
-      <span style={{ width: "10%" }}>
-        <button
-          className={`${styles.button} ${styles.buttonSmall}`}
-          type="button"
-          onClick={() => onRemoveItem(item)}
-        >
+      </StyledColumn>
+      <StyledColumn width="30%">{author}</StyledColumn>
+      <StyledColumn width="10%">{num_comments}</StyledColumn>
+      <StyledColumn width="10%">{points}</StyledColumn>
+      <StyledColumn width="10%">
+        <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
           Dismiss
-        </button>
-      </span>
-    </li>
+        </StyledButtonSmall>
+      </StyledColumn>
+    </StyledItem>
   );
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
       label="Search"
@@ -81,14 +131,10 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
       <strong>Search :</strong>
     </InputWithLabel>
 
-    <button
-      className={`${styles.button} ${styles.buttonLarge}`}
-      type="submit"
-      disabled={!searchTerm}
-    >
+    <StyledButtonLarge type="submit" disabled={!searchTerm}>
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 );
 
 const useSemiPersistenState = (key, initialState) => {
@@ -135,7 +181,6 @@ const storiesReducer = (state, action) => {
 };
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistenState("search", "");
   const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
@@ -181,9 +226,22 @@ const App = () => {
     });
   };
 
+  const StyledContainer = styled.div`
+    height: 100vw;
+    padding: 20px;
+    background: #83a4d4;
+    background: linear-gradient(to left, #b6fbff, #83a4d4);
+    color: #171212;
+  `;
+  const StyledHeadlinePrimary = styled.h1`
+    font-size: 48px;
+    font-weight: 300;
+    letter-spacing: 2px;
+  `;
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -200,7 +258,7 @@ const App = () => {
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </div>
+    </StyledContainer>
   );
 };
 
