9de9086d4dfa4da1f0ef1c0f9a4cb6c983214d4d
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-08 14:53:25 +0700

    Creating reusable input

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 import "./styles.css";
 
-const Search = ({ search, onSearch }) => {
+const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => {
   return (
-    <div>
-      <label htmlFor="search">Search :</label>
-      <input id="search" type="text" value={search} onChange={onSearch} />
-    </div>
+    <>
+      <label htmlFor={id}>{label}</label>
+      &nbsp;
+      <input id={id} type={type} value={value} onChange={onInputChange} />
+    </>
   );
 };
 
@@ -76,7 +77,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
