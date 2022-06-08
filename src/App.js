956bf3725d684f30c60b4e91ea48e7ba8aa740f2
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-08 22:15:45 +0700

    Set searching to hackernews API

--- src/App.js
+++ src/App.js
@@ -116,9 +116,11 @@ const App = () => {
   });
 
   useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -131,7 +133,7 @@ const App = () => {
           type: "STORIES_FETCH_FAILURE"
         });
       });
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -143,10 +145,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
@@ -168,7 +166,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
