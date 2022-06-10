cd0f3d43b894ec2fe5bdc265019443ef47b27781
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-10 08:59:12 +0700

    Testing reducer

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,31 @@
+import App, {
+  storiesReducer,
+  Item,
+  List,
+  SearchForm,
+  InputWithLabel
+} from "./App";
+
+const storyOne = {
+  title: "React",
+  url: "https://reactjs.org/",
+  author: "Jordan Walke",
+  num_comments: 3,
+  points: 4,
+  objectID: 0
+};
+
+const storyTwo = {
+  title: "Redux",
+  url: "https://redux.js.org/",
+  author: "Dan Abramov, Andrew Clark",
+  num_comments: 2,
+  points: 5,
+  objectID: 1
+};
+
+const stories = [storyOne, storyTwo];
+
 describe("something truthy and falsy", () => {
   test("true to be true", () => {
     expect(true).toBeTruthy();
@@ -8,8 +36,21 @@ describe("something truthy and falsy", () => {
   });
 });
 
-describe("App component", () => {
-  test("removes an item when clicking a dismiss button", () => {});
+describe("storiesReducer", () => {
+  test("removes an item when clicking a dismiss button", () => {
+    const action = { type: "REMOVE_STORY", payload: storyOne };
+    const state = { data: stories, isLoading: false, isError: false };
+
+    const newState = storiesReducer(state, action);
+
+    const expectedState = {
+      data: [storyTwo],
+      isLoading: false,
+      isError: false
+    };
+
+    expect(newState).toStrictEqual(expectedState);
+  });
 
   test("request an initial stories from API", () => {});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -366,3 +366,5 @@ const App = () => {
 };
 
 export default App;
+
+export { storiesReducer, SearchForm, InputWithLabel, List, Item };
