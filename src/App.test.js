df3f7105fd60eabca170e7bc8534b7ea1dfe6c5c
Adriana Eka Prayudha <radenadriep@gmail.com>
2022-06-12 00:00:51 +0700

    Completing testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render, screen, fireEvent, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
+import axios from "axios";
 import App, {
   storiesReducer,
   Item,
@@ -9,6 +9,8 @@ import App, {
   SearchForm,
   InputWithLabel
 } from "./App";
+
+jest.mock(axios);
 
 const storyOne = {
   title: "React",
@@ -110,5 +112,132 @@ describe("SearchForm", () => {
     fireEvent.submit(screen.getByRole("button"));
 
     expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  test("renders snapshot", () => {
+    const { container } = render(<SearchForm {...searchFormProps} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
+
+describe("App", () => {
+  test("succeds fetching data", async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: stories
+      }
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<App />);
+
+    expect(screen.queryByText(/Loading/)).toBeInTheDocument();
+
+    await act(() => promise);
+
+    expect(screen.queryByText(/Loading/)).toBeNull();
+  });
+
+  test("fails fetching data", async () => {
+    const promise = Promise.reject();
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<App />);
+
+    expect(screen.getByText(/Loading/)).toBeInTheDocument();
+
+    try {
+      await act(() => promise);
+    } catch (error) {
+      expect(screen.queryByText(/Loading/)).toBeNull();
+      expect(screen.queryByText(/went wrong/)).toBeInTheDocument();
+    }
+  });
+
+  test("removes a story", async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: stories
+      }
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<App />);
+
+    await act(() => promise);
+
+    expect(screen.getAllByText("Dismiss").length).toBe(2);
+    expect(screen.getByText("Jordan Walke")).toBeInTheDocument();
+
+    fireEvent.click(screen.getAllByText("Dismiss")[0]);
+
+    expect(screen.getAllByText("Dismiss").length).toBe(1);
+    expect(screen.queryByText("Jordan Walke")).toBeNull();
+  });
+
+  test("searches for specific stories", async () => {
+    const reactPromise = Promise.resolve({
+      data: {
+        hits: stories
+      }
+    });
+
+    const anotherStory = {
+      title: "JavaScript",
+      url: "https://en.wikipedia.org/wiki/JavaScript",
+      author: "Brendan Eich",
+      num_comments: 15,
+      points: 10,
+      objectID: 3
+    };
+
+    const javascriptPromise = Promise.resolve({
+      data: {
+        hits: [anotherStory]
+      }
+    });
+
+    axios.get.mockImplementation((url) => {
+      if (url.includes("React")) {
+        return reactPromise;
+      }
+
+      if (url.includes("JavaScript")) {
+        return javascriptPromise;
+      }
+      throw Error();
+    });
+
+    // Initial Render
+    render(<App />);
+    // First Data Fetching
+    await act(() => reactPromise);
+
+    expect(screen.queryByDisplayValue("React")).toBeInTheDocument();
+    expect(screen.queryByDisplayValue("JavaScript")).toBeNull();
+    expect(screen.queryByText("Jordan Walke")).toBeInTheDocument();
+
+    expect(screen.queryByText("Dan Abramov, Andrew Clark")).toBeInTheDocument();
+    expect(screen.queryByText("Brendan Eich")).toBeNull();
+
+    // User Interaction -> Search
+    fireEvent.change(screen.queryByDisplayValue("React"), {
+      target: {
+        value: "JavaScript"
+      }
+    });
+    expect(screen.queryByDisplayValue("React")).toBeNull();
+    expect(screen.queryByDisplayValue("JavaScript")).toBeInTheDocument();
+
+    fireEvent.submit(screen.queryByText("Submit"));
+    // Second Data Fetching
+    await act(() => javascriptPromise);
+    expect(screen.queryByText("Jordan Walke")).toBeNull();
+    expect(screen.queryByText("Dan Abramov, Andrew Clark")).toBeNull();
+    expect(screen.queryByText("Brendan Eich")).toBeInTheDocument();
   });
 });
